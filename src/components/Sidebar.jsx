3f571eb2a7dfcd199f0e08d25a99bcad9531c8c6
superquique <enriqueveintidos@gmail.com>
2026-03-10 09:52:52 -0600

    feat: implement sidebar

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,16 @@
+function Sidebar () {
+    return (
+        <div className="sidebar">
+            <ul className="sidebar-menu">
+                <li>
+                    <a href="#index"><h3>Inicio</h3></a>
+                </li>
+                <li>
+                    <a href="#about"><h3>Acerca de</h3></a>
+                </li>
+            </ul>
+        </div>
+    )
+}   
+
+export default Sidebar;
