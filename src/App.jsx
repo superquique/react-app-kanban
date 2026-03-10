f8ec45888d68fb50858f909647c3a25848e27b4e
superquique <enriqueveintidos@gmail.com>
2026-03-10 09:53:47 -0600

    feat: add sidebar and footer to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import './App.css'
 import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import Sidebar from './components/Sidebar'
+
 
 function App() {
 
   return (
-    <>
+    <div className='app-container'>
       <Navbar />
-    </>
+      <Sidebar />
+      <Footer />
+    </div>
   )
 }
 
