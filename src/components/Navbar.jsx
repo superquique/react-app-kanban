70524d1ec66d7908aa8f4a9d2fadbee3225f7a61
superquique <enriqueveintidos@gmail.com>
2026-03-10 08:57:18 -0600

    feat: implement navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+function Navbar () {
+    return (
+        <>
+            <nav className="navbar">
+                <div className="logo">
+                    <h2>React Kanban App </h2>
+                    <img className="logo-img" src="../../public/app-logo.png" />
+                </div>
+            </nav>
+        </>
+    )
+}
+
+export default Navbar;
