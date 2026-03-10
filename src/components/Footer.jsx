99c83516ca7b3d97ecd4231defa719442f2e0b36
superquique <enriqueveintidos@gmail.com>
2026-03-10 09:52:39 -0600

    feat: implement footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+function Footer () {
+    return (
+        <footer>
+            <div className="footer">
+                <a href="https://github.com/superquique/react-app-kanban">
+                https://github.com/superquique/react-app-kanban</a>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
