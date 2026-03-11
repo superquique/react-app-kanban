b7c7c452dd6e9a211f8c6fba800601e25e098da8
superquique <enriqueveintidos@gmail.com>
2026-03-11 10:01:02 -0600

    feat: implement column component

---
+++ src/components/Column.jsx
@@ -0,0 +1,31 @@
+import Task from "./Task";
+
+function Column (props) {
+
+    const filteredTasks = props.taskList.filter((task) => {
+        return task.status === props.columnType;
+    })
+
+    return (
+        <div className="column" style={{backgroundColor: props.bgColorContent}}>
+            <h3 style={{backgroundColor: props.bgColorTitle}}>
+                {props.columnType}
+            </h3>
+
+            { filteredTasks &&
+                filteredTasks.map((taskObj, i, arr) => {
+                    return (
+                        <Task 
+                            taskDetails={taskObj} 
+                            key={taskObj.id} 
+                            bgColor={props.bgColorTask}
+                            deleteCallback={props.deleteTaskCallback}
+                        />
+                    )
+                })
+            }
+        </div>
+    )
+}
+
+export default Column;
