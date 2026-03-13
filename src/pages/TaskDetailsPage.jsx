288197b9309ac4c30a4edfd391670a98c30a310e
superquique <enriqueveintidos@gmail.com>
2026-03-13 10:18:10 -0600

    feat:  implement update task functionality

--- src/pages/TaskDetailsPage.jsx
+++ src/pages/TaskDetailsPage.jsx
@@ -1,18 +1,128 @@
-import { useParams } from "react-router-dom";
+import { useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 
-function TaskDetailsPage ({ taskArr }) {
+function TaskDetailsPage ({ taskArr, onUpdate }) {
     const {taskId} = useParams();
     const taskDetails = taskArr.find((task) => {
         return task.id === taskId
     })
 
-    console.log(taskDetails)
+    const [id, setId] = useState(taskDetails.id);
+    const [title, setTitle] = useState(taskDetails.title);
+    const [description, setDescription] = useState(taskDetails.description);
+    const [assignee, setAssignee] = useState(taskDetails.assignee);
+    const [status, setStatus] = useState(taskDetails.status);
+    const [priority, setPriority] = useState(taskDetails.priority);
+    const [createdDate, setCreatedDate] = useState(taskDetails.createdDate);
+    const [dueDate, setDueDate] = useState(taskDetails.dueDate);
+
+    const navigate = useNavigate();
+
+    const handleSubmit = (e) => {
+        console.log("handlig submit...")
+        e.preventDefault();
+
+        const updatedTask = {
+            id: id,
+            title: title,
+            description: description,
+            assignee: assignee,
+            status: status,
+            priority: priority,
+            createdDate: createdDate,
+            dueDate: dueDate
+        }
+
+        onUpdate(updatedTask);
+
+        navigate("/");
+    }
 
     return (
         <div className="task-page">
             { taskDetails &&
-            <>  
-                <div className="task-header">
+            <div className="task-detail-card">  
+
+                <form onSubmit={handleSubmit} className="form">
+                    <label>
+                        Title
+                        <input 
+                            type="text" 
+                            name="title" 
+                            value={title}
+                            onChange={ e => setTitle(e.target.value) }
+                        />
+                    </label>
+
+                    <label>
+                        Description
+                        <input 
+                            type="text" 
+                            name="description" 
+                            value={description}
+                            onChange={ e => setDescription(e.target.value) }
+                        />
+                    </label>
+
+                    <label>
+                        Assignee
+                        <input 
+                            type="text" 
+                            name="assignee" 
+                            value={assignee}
+                            onChange={ e => setAssignee(e.target.value) }
+                        />
+                    </label>
+
+                    <label>
+                        Status
+                        <select 
+                            name="status"
+                            value={status}
+                            onChange={ e => setStatus(e.target.value) }
+                        >
+                            <option value="To Do">To Do</option>
+                            <option value="In Progress">In Progress</option>
+                            <option value="Done">Done</option>
+                        </select>
+                    </label>
+
+                    <label>
+                        Priority
+                        <select 
+                            name="priority"
+                            value={priority}
+                            onChange={ e => setPriority(e.target.value) }
+                        >
+                            <option value="Low">Low</option>
+                            <option value="Medium">Medium</option>
+                            <option value="High">High</option>
+                        </select>
+                    </label>
+
+                    <label>
+                        Created date
+                        <input 
+                            type="date" 
+                            name="created-date" 
+                            value={createdDate}
+                            onChange={ e => setCreatedDate(e.target.value) }
+                        />
+                    </label>
+
+                    <label>
+                        Due date
+                        <input 
+                            type="date" 
+                            name="due-date"
+                            value={dueDate}
+                            onChange={ e => setDueDate(e.target.value) }
+                        />
+                    </label>
+                    
+                    <button>Save</button>
+                </form>
+                {/* <div className="task-header-detail">
                     <div className="task-header-child">        
                         <h2>
                             {taskDetails.title}
@@ -78,8 +188,8 @@ function TaskDetailsPage ({ taskArr }) {
                         </div>
                     </div>
                     
-                </div>
-            </>
+                </div> */}
+            </div>
             }
         </div>
     )
